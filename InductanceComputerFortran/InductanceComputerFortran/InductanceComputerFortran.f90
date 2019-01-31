40432a771bced06ea497599b72950d07685ba48b
grgsiberia <nanashi4129@gmail.com>
2019-01-31 18:35:16 +0900

    重積分の回数を合わせようとするもわからん

--- InductanceComputerFortran/InductanceComputerFortran/InductanceComputerFortran.f90
+++ InductanceComputerFortran/InductanceComputerFortran/InductanceComputerFortran.f90
@@ -12,20 +12,54 @@
 !
 !****************************************************************************
 
-    program InductanceComputerFortran
+program InductanceComputerFortran
 
     implicit none
 
-    double precision, dimension(3) :: x
-    double precision a
-    a = 5
-    x = (/ 3, 3, 3 /)
-    x = x * a
+    ! ïœêîêÈåæ
+    integer numof_wires, numof_coils
+    numof_wires = 100
+    numof_coils = 2
+
+    ! Main ÇÃñ{ï∂
+    CALL Main(numof_wires, numof_coils)
     
-    ! ïœêî
+contains
+    subroutine Main(numof_wires, numof_coils)
+        USE WiredFluxDensity
+        implicit none
+        integer, intent(in) :: numof_wires, numof_coils
+        double precision, dimension(numof_wires, 3) :: wire_positions
+        double precision, dimension(numof_coils, 3) :: coil_positions, coil_forwards, coil_rights
+        double precision, dimension(numof_coils) :: coil_heights, coil_radius
+        double precision sigma, gamma
+        double precision, dimension(numof_wires) :: wired_flux_densities
+        
+        integer i, numof_dradius, numof_dtheta
+        
+        DO i = 1, numof_wires
+            wire_positions(i,:) = (/ 0.5d0 * i - 25, 0.0d0, 5.0d0 /)
+        END DO
+        
+        coil_positions(1,:) = (/ 0.0d0, 3.0d0, 0.0d0 /)
+        coil_positions(2,:) = -coil_positions(1,:)
+        
+        DO i = 1, numof_coils
+            coil_forwards(i,:) = (/ 0.0d0, 0.0d0, 1.0d0 /)
+            coil_rights(i,:) = (/ 1.0d0, 0.0d0, 0.0d0 /)
+        END DO
+        
+        coil_heights = 1.0d0
+        coil_radius = 1.0d0
+        sigma = 1.0d0
+        gamma = 1.0d0
+        numof_dradius = 8
+        numof_dtheta = 6
+        
+        wired_flux_densities = wired_flux_density_on_coil(numof_wires, wire_positions, numof_coils, coil_positions, coil_forwards, coil_rights, coil_heights, coil_radius, 1.0d0, numof_dtheta, numof_dradius)
+        
+        print *, wired_flux_densities
+    end subroutine
 
-    ! InductanceComputerFortran ÇÃñ{ï∂
-    print *, 'Hello World'
-
-    end program InductanceComputerFortran
+end program InductanceComputerFortran
 
