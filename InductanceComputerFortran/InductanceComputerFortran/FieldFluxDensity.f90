824b861df701df3217a2133c8e07b8876ed21da4
grgsiberia <nanashi4129@gmail.com>
2019-02-01 19:25:28 +0900

    空間の磁束密度を求める

--- InductanceComputerFortran/InductanceComputerFortran/FieldFluxDensity.f90
+++ InductanceComputerFortran/InductanceComputerFortran/FieldFluxDensity.f90
@@ -55,4 +55,44 @@ contains
         
     end function
     
+    function get_measure_point3D(x, y, z, field_delta, origin)
+        implicit none
+        integer, intent(in) :: x, y, z
+        double precision, dimension(3), intent(in) :: field_delta, origin
+        double precision, dimension(3) :: get_measure_point3D
+        
+        get_measure_point3D = (/ DBLE(x), DBLE(y), DBLE(z) /)
+        get_measure_point3D = get_measure_point3D * field_delta + origin
+        
+    end function
+    
+    function field_flux_density(origin, numof_field, field_size, numof_wire, wire_positions, wired_flux_densities, numof_coil, coil_forwards, gamma)
+        implicit none
+        integer, intent(in) :: numof_wire, numof_coil
+        double precision, dimension(3), intent(in) :: origin, field_size, 
+        double precision, dimension(numof_wire), intent(in) :: wired_flux_densities
+        double precision, dimension(numof_wire, 3), intent(in) :: wire_positions
+        double precision, dimension(numof_coil, 3), intent(in) :: coil_forwards
+        integer, dimension(3), intent(in) :: numof_field
+        double precision, intent(in) :: gamma
+        double precision, dimension(3) :: measure_point, field_delta
+        double precision, dimension(numof_field(1), numof_field(2), numof_field(3)) :: field_flux_density
+        
+        integer i, x, y, z
+        
+        DO i = 1, 3
+            field_delta(i) = field_size(i) / DBLE(numof_field(i))
+        END DO
+        
+        DO x = 1, numof_field(1)
+            DO y = 1, numof_field(2)
+                DO z = 1, numof_field(3)
+                    measure_point = get_measure_point3D(x, y, z, field_delta, origin)
+                    field_flux_density(x,y,z) = compute_inducted_wire(measure_point, numof_wire, wire_positions, wired_flux_densities, numof_coil, coil_forwards, gamma)
+                END DO
+            END DO
+        END DO
+        
+    end function
+    
 end module
