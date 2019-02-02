d95ec865858ebfa515c873400631c1b05cb1fd37
grgsiberia <nanashi4129@gmail.com>
2019-02-02 17:17:47 +0900

    プリントする

--- InductanceComputerFortran/InductanceComputerFortran/InductanceComputerFortran.f90
+++ InductanceComputerFortran/InductanceComputerFortran/InductanceComputerFortran.f90
@@ -9,23 +9,27 @@ program InductanceComputerFortran
     implicit none
 
     ! ïœêîêÈåæ
-    integer numof_wires, numof_coils
-    numof_wires = 100
-    numof_coils = 2
+    integer, parameter :: numof_wires = 100, numof_coils = 2
+    integer, parameter, dimension(3) :: numof_size = (/ 200, 200, 200 /)
 
     ! Main ÇÃñ{ï∂
-    CALL Main(numof_wires, numof_coils)
+    CALL Main(numof_wires, numof_coils, numof_size)
     
 contains
-    subroutine Main(numof_wires, numof_coils)
+    subroutine Main(numof_wires, numof_coils, numof_size)
         USE WiredFluxDensity
+        USE FieldFluxDensity
         implicit none
         integer, intent(in) :: numof_wires, numof_coils
+        integer, dimension(3), intent(in) :: numof_size
+        
         double precision, dimension(numof_wires, 3) :: wire_positions
         double precision, dimension(numof_coils, 3) :: coil_positions, coil_forwards, coil_rights
         double precision, dimension(numof_coils) :: coil_heights, coil_radius
+        double precision, dimension(3) :: origin, field_size, field_right, field_forward
         double precision sigma, gamma
         double precision, dimension(numof_wires) :: wired_flux_densities
+        double precision, dimension(numof_size(1), numof_size(2), numof_size(3)) :: field_flux_densities
         
         integer i, numof_dradius, numof_dtheta
         
@@ -41,6 +45,11 @@ contains
             coil_rights(i,:) = (/ 1.0d0, 0.0d0, 0.0d0 /)
         END DO
         
+        origin = (/ -25.0d0, -25.0d0, -25.0d0 /)
+        field_size = (/ 50.0d0, 50.0d0, 50.0d0 /)
+        field_right = (/ 1.0d0, 0.0d0, 0.0d0 /)
+        field_forward = (/ 0.0d0, 0.0d0, 1.0d0 /)
+        
         coil_heights = 1.0d0
         coil_radius = 1.0d0
         sigma = 1.0d0
@@ -48,11 +57,12 @@ contains
         numof_dradius = 100
         numof_dtheta = 100
         
-        wired_flux_densities = wired_flux_density_on_coil(numof_wires, wire_positions, numof_coils, coil_positions, coil_forwards, coil_rights, coil_heights, coil_radius, 1.0d0, numof_dtheta, numof_dradius)
+        wired_flux_densities = wired_flux_density_on_coil(numof_wires, wire_positions, numof_coils, coil_positions, coil_forwards, coil_rights, coil_heights, coil_radius, sigma, numof_dtheta, numof_dradius)
+        field_flux_densities = field_flux_density(origin, numof_size, field_size, field_forward, field_right, numof_wires, wire_positions, wired_flux_densities, numof_coils, coil_forwards, gamma)
         
         print *, wired_flux_densities
         CALL PrintWiredFluxDensity(numof_wires, wired_flux_densities)
-        
+        CALL PrintFieldFluxDensity(numof_size, field_flux_densities)
     end subroutine
     
     subroutine PrintWiredFluxDensity(numof_wires, wired_flux_densities)
@@ -66,6 +76,28 @@ contains
         
         DO i = 1, numof_wires
             write (FD, *) wired_flux_densities(i)
+        END DO
+        
+        CLOSE(FD)
+    end subroutine
+    
+    subroutine PrintFieldFluxDensity(numof_size, field_flux_densities)
+        implicit none
+        integer, dimension(3), intent(in) :: numof_size
+        double precision, dimension(numof_size(1), numof_size(2), numof_size(3)), intent(in) :: field_flux_densities
+        integer, parameter :: FD = 18
+        integer i, j, k
+        
+        OPEN(FD, file="field_flux_density.csv", status="replace")
+        
+        write (FD, *) numof_size(1), numof_size(2), numof_size(3)
+        
+        DO i = 1, numof_size(1)
+            DO j = 1, numof_size(2)
+                DO k = 1, numof_size(3)
+                    write (FD, *) field_flux_densities(i,j,k)
+                END DO
+            END DO
         END DO
         
         CLOSE(FD)
