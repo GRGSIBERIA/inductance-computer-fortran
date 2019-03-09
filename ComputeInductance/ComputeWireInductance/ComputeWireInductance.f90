f03a9444d240f52c9bd874d2a8829491aa4e7cc8
grgsiberia <nanashi4129@gmail.com>
2019-03-09 18:56:08 +0900

    とりあえずINPファイルの読み込みを終わらせる

--- ComputeInductance/ComputeWireInductance/ComputeWireInductance.f90
+++ ComputeInductance/ComputeWireInductance/ComputeWireInductance.f90
@@ -22,7 +22,15 @@
     
     ! メイン関数
     subroutine Main()
-    
+        use InputFileClass
+        
+        implicit none
+        integer, parameter :: fd = 20
+        type(InputFile) input
+        
+        open(fd, file="E:\\temp\\rhodes\\odb\\C3-17-Gapped-Detail-C2600.inp", status="old")
+        
+        input = init_InputFile(fd, "coil")
     end subroutine
     
     end program ComputeWireInductance
