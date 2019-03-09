44fd45f64b971d7d433b372e74301c6480755f3a
grgsiberia <nanashi4129@gmail.com>
2019-03-09 16:41:44 +0900

    プロジェクトの整理

---
+++ ComputeInductance/ComputeInductance/ComputeInductance.f90
@@ -0,0 +1,25 @@
+!  ComputeInductance.f90 
+!
+!  関数:
+!  ComputeInductance - コンソール・アプリケーションのエントリーポイント。
+!
+
+!****************************************************************************
+!
+!  プログラム: ComputeInductance
+!
+!  目的:  コンソール・アプリケーションのエントリーポイント。
+!
+!****************************************************************************
+
+    program ComputeInductance
+
+    implicit none
+
+    ! 変数
+
+    ! ComputeInductance の本文
+    print *, 'Hello World'
+
+    end program ComputeInductance
+
---
+++ ComputeInductance/ComputeWireInductance/ComputeWireInductance.f90
@@ -0,0 +1,29 @@
+﻿!  ComputeWireInductance.f90 
+!
+!  関数:
+!  ComputeWireInductance - コンソール・アプリケーションのエントリーポイント。
+!
+
+!****************************************************************************
+!
+!  プログラム: ComputeWireInductance
+!
+!  目的:  コンソール・アプリケーションのエントリーポイント。
+!
+!****************************************************************************
+
+    program ComputeWireInductance
+
+    implicit none
+
+    CALL Main()
+    
+    contains
+    
+    ! メイン関数
+    subroutine Main()
+    
+    end subroutine
+    
+    end program ComputeWireInductance
+
