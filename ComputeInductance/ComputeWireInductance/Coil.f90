84e247fcf3eb6c53af8b5e1fd3d127a0aed6d50f
grgsiberia <nanashi4129@gmail.com>
2019-03-12 18:36:37 +0900

    ファイルの入力を済ませる

---
+++ ComputeInductance/ComputeWireInductance/Coil.f90
@@ -0,0 +1,18 @@
+﻿    module CoilClass
+    
+    type Coil
+        real :: height, radius, gamma
+        integer :: forwardNodeId, backwardNodeId
+    end type
+    
+    interface Coil
+        module procedure :: init_Coil
+    end interface
+    
+    contains
+    
+    type(Coil) function init_Coil() result(this)
+        
+    end function
+    
+    end module
--- ComputeInductance/ComputeWireInductance/ConfigFile.f90
+++ ComputeInductance/ComputeWireInductance/ConfigFile.f90
@@ -2,9 +2,11 @@
         implicit none
         
         type Config
+            integer :: numofCoils
             integer :: configFD
             integer :: inputFD
-            integer, dimension(:), allocatable :: wireFDs, coilFDs, frontFDs
+            integer, dimension(:), allocatable :: wireFDs
+            integer, dimension(:), allocatable :: topFDs, bottomFDs
         end type
         
         interface Config
@@ -15,61 +17,94 @@
     
         type(Config) function init_Config(startFD, configPath) result(this)
             character(*), intent(in) :: configPath
-            integer, intent(in) :: startFD
+            integer :: startFD
             
             character*32    option
-            character*256   param
-            integer wireCount, coilCount, frontCount, inputCount
+            character*256   param, topfile, bottomfile
+            character*1024  line, also
+            integer wireCount, coilCount, inputCount
+            integer numofWires, numofCoils, numofInputs
             
             this%configFD = startFD
             startFD = startFD + 1
             
             OPEN (this%configFD, file=configPath, status="old")
             
-            inputCount = 0
-            wireCount = 0
-            coilCount = 0
-            frontCount = 0
+            numofInputs = 0
+            numofWires = 0
+            numofCoils = 0
             
             ! ファイルの数を数える
             do
-                READ (startFD, *, end=100) option, param
+                READ (this%configFD, *, end=100) option, param
                 
                 if (INDEX(option, "wirefile") > 0) then
-                    wireCount = wireCount + 1
+                    numofWires = numofWires + 1
                 elseif (INDEX(option, "coilfile") > 0) then
-                    coilCount = coilCount + 1
-                elseif (INDEX(option, "frontfile") > 0) then
-                    frontCount = frontCount + 1
+                    numofCoils = numofCoils + 1
                 elseif (INDEX(option, "inputfile") > 0) then
-                    inputCount = inputCount + 1
+                    numofInputs = numofInputs + 1
                 end if
             end do
 100         continue
             
             ! ここからエラー処理
-            if (frontCount /= coilCount) then
-                PRINT *, "The number of coilfile does not match the number of frontfile."
-                PRINT *, "Please declare the same the number of coilfile and the number of frontfile in the config file."
-                STOP
-            end if
-            
-            if (inputCount > 1) then
+            if (numofInputs > 1) then
                 PRINT *, "Please declare only one input file."
-                PRINT *, "The number of inputfiles > ", inputCount
-            elseif (inputCount <= 0) then
+                PRINT *, "The number of inputfiles > ", numofInputs
+            elseif (numofInputs <= 0) then
                 PRINT *, "Please declare an inputfile."
                 STOP
             end if
             ! エラー処理ここまで
             
-            ALLOCATE (this%wireFDs(wireCount))
-            ALLOCATE (this%coilFDs(coilCount))
-            ALLOCATE (this%frontFDs(frontCount))
+            this%numofCoils = numofCoils
+            ALLOCATE (this%wireFDs(numofWires))
+            ALLOCATE (this%topFDs(numofCoils))
+            ALLOCATE (this%bottomFDs(numofCoils))
             
             ! ファイルパスを読み込んでコンフィグに記帳する
             
-            ! Coilクラスを作ってheight, radius, gammaを宣言したほうがいいような気がする
+            ! Coil上面と下面でファイルを分けてもらう方針しかないだろう^
+            ! coilfile, 上面XYデータ, 下面XYデータ
+            
+            REWIND (this%configFD)
+            
+            coilCount = 1
+            wireCount = 1
+            inputCount = 1
+            
+            do
+                READ (this%configFD, "(A)", end=200) line
+                
+                READ (line, *) option, also
+                
+                if (INDEX(option, "wirefile") > 0) then     ! ワイヤの読み込み
+                    READ (line, *) option, param
+                    OPEN (startFD, file=param, status="old")
+                    this%wireFDs(wireCount) = startFD
+                    wireCount = wireCount + 1
+                    startFD = startFD + 1
+                    
+                elseif(INDEX(option, "coilfile") > 0) then  ! 上面，下面の順番で記帳
+                    READ (line, *) option, topfile, bottomfile
+                    OPEN (startFD, file=topfile, status="old")
+                    OPEN (startFD+1, file=bottomfile, status="old")
+                    this%topFDs(coilCount) = startFD
+                    this%bottomFDs(coilCount) = startFD + 1
+                    coilCount = coilCount + 1
+                    startFD = startFD + 2
+                    
+                elseif(INDEX(option, "inputfile") > 0) then ! 入力ファイルの読み込み
+                    READ (line, *) option, param
+                    OPEN (startFD, file=param, status="old")
+                    this%inputFD = startFD
+                    startFD = startFD + 1
+                end if
+                
+            end do
+200         continue            
+            
         end function
     end module
     
