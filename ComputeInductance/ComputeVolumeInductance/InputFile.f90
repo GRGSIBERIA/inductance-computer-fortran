ba3078f582b754655add56a55cddfe935afdec2a
grgsiberia <nanashi4129@gmail.com>
2019-04-06 15:01:48 +0900

    ほげ

--- ComputeInductance/ComputeVolumeInductance/InputFile.f90
+++ ComputeInductance/ComputeVolumeInductance/InputFile.f90
@@ -9,25 +9,25 @@
     
     contains
     
-    type(InputFile) function init_InputFile(fd, part) result(this)
+    integer function CountNodePositionInFile(fd, part, localPosition) result(count)
         implicit none
+        double precision, dimension(3), intent(inout) :: localPosition
         integer, intent(in) :: fd
         character(*), intent(in) :: part
-        
         character*128 line
-        integer count
-        
-        this%localPosition = 0.0d0
+        count = 0
         
         REWIND (fd)
         
         do
 200         continue            
             READ (fd, "(A)", end=100) line
+            count = count + 1
             
             ! Partがきたなら次にNodeだと本体が来る
             if (INDEX(line, "*Part") > 0 .and. INDEX(line, part) > 0) then
                 READ (fd, "(A)", end=100) line
+                count = count + 1
                 
                 if (INDEX(line, "*Node") > 0) then
                     goto 100        ! 本体が来る
@@ -35,13 +35,14 @@
                 
             else if (INDEX(line, "*Instance") > 0 .and. INDEX(line, part) > 0) then
                 READ (fd, "(A)", end=100) line
+                count = count + 1
                 
                 if (INDEX(line, "*End") > 0) then
                     goto 200                            ! 何もないので読み込みを繰り返す
                 else if (INDEX(line, "*Node") > 0) then
                     goto 100                            ! 本体が来る
                 else
-                    READ (fd, *) this%localPosition(:)  ! 局所座標の代入以外は何もなし
+                    READ (fd, *) localPosition(:)  ! 局所座標の代入以外は何もなし
                     goto 200
                 end if
                 
@@ -50,6 +51,17 @@
         end do
 100     continue
         
+    end function
+    
+    type(InputFile) function init_InputFile(fd, part) result(this)
+        implicit none
+        integer, intent(in) :: fd
+        character(*), intent(in) :: part
+        
+        character*128 line
+        integer nodePosition
+        
+        nodePosition = CountNodePositionInFile(fd, part, this%localPosition)
         
         
     end function
