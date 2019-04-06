3f38a4f71c6540e3dff1298761441f92c13a7353
grgsiberia <nanashi4129@gmail.com>
2019-04-06 14:14:21 +0900

    もはや別プロジェクトでよい

---
+++ ComputeInductance/ComputeVolumeInductance/ComputeVolumeInductance.f90
@@ -0,0 +1,25 @@
+!  ComputeVolumeInductance.f90 
+!
+!  関数:
+!  ComputeVolumeInductance - コンソール・アプリケーションのエントリーポイント。
+!
+
+!****************************************************************************
+!
+!  プログラム: ComputeVolumeInductance
+!
+!  目的:  コンソール・アプリケーションのエントリーポイント。
+!
+!****************************************************************************
+
+    program ComputeVolumeInductance
+
+    implicit none
+
+    ! 変数
+
+    ! ComputeVolumeInductance の本文
+    print *, 'Hello World'
+
+    end program ComputeVolumeInductance
+
---
+++ ComputeInductance/ComputeVolumeInductance/InputFile.f90
@@ -0,0 +1,3 @@
+﻿    module InputFileClass
+    
+    end module
