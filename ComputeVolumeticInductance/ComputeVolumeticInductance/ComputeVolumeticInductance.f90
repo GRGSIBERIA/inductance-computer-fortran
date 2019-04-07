e1c99d9b4c64e92f6fb0c5c2d3a15c6d596e2307
grgsiberia <nanashi4129@gmail.com>
2019-04-07 19:53:43 +0900

    とりあえず，ヘッダの読み込み部分のテストが完了した

--- ComputeVolumeticInductance/ComputeVolumeticInductance/ComputeVolumeticInductance.f90
+++ ComputeVolumeticInductance/ComputeVolumeticInductance/ComputeVolumeticInductance.f90
@@ -25,21 +25,28 @@
         USE FileUtil
         USE InputFileClass
         USE CommonReportClass
+        USE ReportFileClass
         
         implicit none
         integer, parameter :: fd = 20, comfd = 21
         
         type(InputFile) inp
         type(CommonReport) com
+        type(ReportFile) report
         
         character*128, dimension(:), allocatable :: inpLines
     
         OPEN (fd, file="E:/temp/rhodes/odb/C3-17-Gapped-Detail-S45C.inp", status="old")
+        
         CALL GetLines(fd, inpLines)
         inp = init_InputFile(inpLines, "coil")
         
         OPEN (comfd, file="E:/temp/rhodes/odb/tine.rpt", status="old")
         com = init_CommonReport(comfd)
+        report = init_ReportFile(comfd, inp, com)
+        
+        DEALLOCATE (inpLines)
+        
         
         CLOSE (fd)
         CLOSE (comfd)
--- ComputeVolumeticInductance/ComputeVolumeticInductance/ReportFile.f90
+++ ComputeVolumeticInductance/ComputeVolumeticInductance/ReportFile.f90
@@ -8,25 +8,61 @@
     
     contains
     
+    ! ヘッダを探索してその文字列を返す手続き
+    subroutine ScanHeader(lines, num, header)
+        character*128, dimension(:), intent(in) :: lines
+        integer, intent(inout) :: num
+        character*128, intent(out) :: header
+        integer xpos, i
+        
+        header = ""
+        
+        ! Xが存在する場所を探す
+        do
+            num = num + 1
+            xpos = INDEX(lines(num), "  X  ")
+            if (xpos > 0) then
+                goto 100
+            end if
+        end do
+100     continue
+        
+        header = TRIM(ADJUSTL(lines(num)(xpos+5:)))
+        
+        ! 戻る方向に探索しつつ，文字列を結合する
+        i = 1
+        do
+            if (LEN_TRIM(ADJUSTL(lines(num-i))) < 1) then
+                goto 200
+            end if
+            header = TRIM(ADJUSTL(lines(num-i))) // header
+            i = i + 1
+        end do
+200     continue
+    end subroutine
+    
+    
     
     ! 個別のレポートファイルのデータ
-    type(ReportFile) function init_ReportFile(fd, part, input, com) result(this)
+    type(ReportFile) function init_ReportFile(fd, input, com) result(this)
         USE FileUtil
         USE InputFileClass
         USE CommonReportClass
     
         implicit none
         integer, intent(in) :: fd
-        character*32, intent(in) :: part
         character*128, dimension(:), allocatable :: lines
         type(InputFile), intent(in) :: input
         type(CommonReport), intent(in) :: com
         
+        character*128 header    ! テスト
+        integer num
+        num = 1
+        
         CALL GetLines(fd, lines)
+        CALL ScanHeader(lines, num, header)
         
-        this%partName = part
-        
-        
+        DEALLOCATE (lines)
         
     end function
     
