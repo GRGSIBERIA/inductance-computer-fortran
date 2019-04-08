ce425f236c5f717701141191d8f3dd312a4e7f24
grgsiberia <nanashi4129@gmail.com>
2019-04-08 19:20:29 +0900

    本日の作業は終わり

---
+++ ComputeVolumeticInductance/ComputeVolumeticInductance/Math.f90
@@ -0,0 +1,75 @@
+﻿    module Math
+    implicit none
+    
+    contains
+    
+    ! 外積
+    function Cross(A, B)
+        implicit none
+        double precision, dimension(3), intent(in) :: A, B
+        double precision, dimension(3) :: Cross
+        
+        double precision, dimension(3) :: A1, A2, B1, B2
+        A1 = (/ A(2), A(3), A(1) /)
+        A2 = (/ A(3), A(1), A(2) /)
+        B1 = (/ B(3), B(1), B(2) /)
+        B2 = (/ B(2), B(3), B(1) /)
+        
+        Cross = A1 * B1 - A2 * B2
+    end function
+    
+    ! クォータニオンの実数部を求める
+    double precision function MulQuaternion_R(Ar, Ai, Br, Bi) result(retR)
+        implicit none
+        double precision, dimension(3), intent(in) :: Ai, Bi
+        double precision, intent(in) :: Ar, Br
+        
+        retR = Ar * Br - DOT_PRODUCT(Ai, Bi)
+    end function
+    
+    ! クォータニオンの虚数部を求める
+    function MulQuaternion_I(Ar, Ai, Br, Bi) result(retI)
+        implicit none
+        double precision, dimension(3), intent(in) :: Ai, Bi
+        double precision, dimension(3) :: retI
+        double precision, intent(in) :: Ar, Br
+        
+        retI = Ar * Bi + Ai * Br + Cross(Ai, Bi)
+    end function
+    
+    ! ベクトルの長さを返す
+    function Length(V)
+        implicit none
+        double precision ,dimension(3), intent(in) :: V
+        double precision Length
+        Length = SQRT(DOT_PRODUCT(V, V))
+    end function
+    
+    function Length4(V) result(L)
+        implicit none
+        double precision, dimension(4), intent(in) :: V
+        double precision L
+        L = SQRT(DOT_PRODUCT(V, V))
+    end function
+    
+    subroutine Normalize4(R, I)
+        implicit none
+        double precision, dimension(3), intent(inout) :: I
+        double precision, intent(inout) :: R
+        double precision, dimension(4) :: Q
+        Q = (/ I(:), R /)
+        Q = Q / Length4(Q)
+        I = Q(1:3)
+        R = Q(4)
+    end subroutine
+    
+    ! ベクトルの正規化
+    function Normalize(V)
+        implicit none
+        double precision, dimension(3), intent(in) :: V
+        double precision, dimension(3) :: Normalize
+        
+        Normalize = 1.0d0 / Length(V) * V
+    end function
+    
+    end module
--- ComputeVolumeticInductance/ComputeVolumeticInductance/Oscillator.f90
+++ ComputeVolumeticInductance/ComputeVolumeticInductance/Oscillator.f90
@@ -4,31 +4,95 @@
     ! 振動体クラス
     type Oscillator
         character*32 partName
-        double precision, dimension(:,:,:), allocatable :: positions     ! パート単位でレポートの座標値
+        integer numofTimes, numofElements, numofNodes, numofElements
+        double precision, dimension(:,:,:), allocatable :: positions    ! パート単位でレポートの座標値
+        integer, dimension(:,:), allocatable :: elements
+        double precision, dimension(:,:,:), allocatable :: centroids     ! 重心
+        double precision, dimension(:,:), allocatable :: volumes         ! 体積
     end type
     
     contains
     
+    
+    ! 体積を返す関数
+    double precision function ComputeVolume(a,b,c,d) result(volume)
+        USE Math
+        implicit none
+        double precision, dimension(3), intent(in) :: a, b, c, d
+        volume = (Length(b-a) * Length(c-a) * Length(d-a)) / 3.0d0
+    end function
+    
+    
+    ! 重心を設定する関数
+    subroutine SetCentroidAndVolume(centroid, volumes, numofElements, elements, numofTimes, numofNodes, positions)
+        implicit none
+        integer, intent(in) :: numofElements, numofTimes, numofNodes
+        double precision, dimension(3,numofTimes,numofElements), intent(out) :: centroids
+        double precision, dimension(numofTimes,numofElements), intent(out) :: volumes
+        double precision, dimension(3,numofTimes,numofNodes), intent(in) :: positions
+        integer, dimension(4,numofElements), intent(in) :: elements
+        
+        integer eid, vid, tid
+        
+        centroids = 0
+        
+        ! 重心を求めるために要素節点から節点の座標値を足し合わせる
+        do eid = 1, numofElements
+            do tid = 1, numofTimes    
+                do vid = 1, 4
+                    centroids(:,tid,eid) = centroids(:,tid,eid) + positions(:,tid,elements(vid,eid))
+                end do
+            end do
+        end do
+        
+        do eid = 1, numofElements
+            do tid = 1, numofTimes
+                volumes(tid,eid) = ComputeVolume(&
+                    positions(:,tid,elements(1,eid)), &
+                    positions(:,tid,elements(2,eid)), &
+                    positions(:,tid,elements(3,eid)), &
+                    positions(:,tid,elements(4,eid)))
+            end do
+        end do
+        
+        ! 4点の平均を取って終わり
+        centroids = centroids / 4
+    end subroutine
+    
+    
+    
     ! numofNodes は maximumNodeIdと同じ数を宣言している
-    type(Oscillator) function init_Oscillator(part, reports, numofTimes, numofNodes) result(this)
+    ! inputもパートごとに存在しているので取扱に注意する
+    type(Oscillator) function init_Oscillator(part, reports, input, com) result(this)
+        USE InputFileClass
+        USE CommonReportClass
         USE ReportFileClass
         implicit none
         character*32, intent(in) :: part
         type(ReportFile), dimension(:), intent(in) :: reports
-        integer, intent(in) :: numofTimes, numofNodes
+        type(InputFile), intent(in) :: input
+        type(CommonReport), intent(in) :: com
+        
         integer i
         
         this%partName = part
         ALLOCATE (this%positions(3,numofTimes, numofNodes))
         this%positions = 0
+        this%numofTimes = com%numofTimes
+        this%numofNodes = input%maximumNodeId       ! 要素節点として登録されている最大値を代入
+        this%numofElements = input%numofElements
+        this%elements = input%elements
         
         do i = 1, SIZE(reports)
-            if (part == reports(i)%partName) then
+            if (part == reports(i)%partName .and. part == input%partName) then
                 this%positions = this%positions + reports(i)%positions
             end if
         end do
         
+        ALLOCATE (this%centroids(3,this%numofTimes,this%numofElements))
+        ALLOCATE (this%volumes(this%numofTimes,this%numofElements))
         
+        ! まだ重心は設定してないよ！
     end function
     
     end module
